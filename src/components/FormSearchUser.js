6c4d8a0be7a7f12ba0df1fd19a337b90a296907a
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-08-16 18:37:05 -0300

    feat: form search user

---
+++ src/components/FormSearchUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { makeGet } from '../helpers/api';
+
+export default function FormSearchUser() {
+  return (
+    <form action="">
+      <button type="button">Novo</button>
+      <input type="text" name="" placeholder="Pesquisar Usuários" id=""/>
+      <button type="button">Atualizar</button>
+    </form>
+  )
+}
