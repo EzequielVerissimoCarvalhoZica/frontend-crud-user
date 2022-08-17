90c18aace8aae3a0ee09e0a8a75b6a9b0e7f8897
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-08-17 10:33:57 -0300

    feat: search user input and reload users

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { makeGet } from '../helpers/api';
 import UserCard from './UserCard';
 
 export default function UsersTable() {
   const [users, setUsers] = useState([]);
+  const [usersFiltered, setUsersFiltered] = useState([]);
   const [error, setError] = useState();
-
+  const reload = useSelector((state) => state.searchedUser.reload)
 
   useEffect(() => {
     async function getOrderDatail() {
@@ -14,14 +16,21 @@ export default function UsersTable() {
       if (response.status !== 200) setError({ status: response.status, message: response.statusText})
       
       setUsers(response.data)
+      setUsersFiltered(response.data)
     }
     getOrderDatail();
-  }, [])
+  }, [reload])
+
+  const searched = useSelector((state) => state.searchedUser.search)
+  useEffect(() => {
+    const newUsers = users.filter(({ name }) => name.toLowerCase().includes(searched))
+    setUsersFiltered(newUsers)
+  }, [searched])
   return (
     <>
       {
         error ?
-        <span>error</span>:
+        <span>{`${error.status} ${error.message}`}</span>:
         <table>
           <thead>
             <tr>
@@ -33,7 +42,7 @@ export default function UsersTable() {
             </tr>
           </thead>
           <tbody>
-            <UserCard  allUsers={users} />
+            <UserCard  allUsers={usersFiltered} />
           </tbody>
         </table>
       }
