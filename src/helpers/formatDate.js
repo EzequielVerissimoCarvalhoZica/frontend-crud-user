dfe9f5d08cb6f8d6adc8ef9fd8237009231a0ee9
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-08-17 21:28:21 -0300

    feat: format date mettod

---
+++ src/helpers/formatDate.js
@@ -0,0 +1,8 @@
+export const formatDate = (date) => {
+  if(typeof date !== 'string') return
+  const MAX_INDEX = 10;
+  const result = date.substring(0, MAX_INDEX).split('-');
+  const dateFormated = `${result[2]}/${result[1]}/${result[0]}`;
+  
+  return dateFormated
+};
